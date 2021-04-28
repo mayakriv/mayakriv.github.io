830a2e3e8e6a8332366e57772a2b961e19f6a4f9
mayakriv <64095758+mayakriv@users.noreply.github.com>
2021-04-28 23:41:04 +0300

    Delete sitepages directory

--- sitepages/topic-list/topic-list.component.js
+++
@@ -1,8 +0,0 @@
-angular.
-  module('designApp').
-  component('topicList', {
-    templateUrl: 'app/topic-list/topic-list.template.html',
-    controller: function TopicListController(Topic, $scope) {
-      $scope.topics = Topic.query();
-    }
-  });
